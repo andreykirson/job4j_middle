052cbc2e37e1951b392e2b077b94285fcc80c2bc
fruit <a.s.kirson@gmail.com>
2020-10-31 19:36:44 +1000

    4.1. JSON формат. [#364789]

--- src/main/java/servlet/JSONServlet.java
+++ src/main/java/servlet/JSONServlet.java
@@ -6,9 +6,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.json.JSONObject;
+import org.json.JSONPointer;
 
 /**
  * @author Andrey
@@ -21,11 +27,21 @@ public class JSONServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        Gson gson = new Gson();
-        String json = gson.toJson(req.getParameter("text"), String.class);
-        System.out.println(json);
-        PrintWriter out = resp.getWriter();
-        out.println(json);
-        out.flush();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (BufferedReader reader = req.getReader();
+            PrintWriter writer = resp.getWriter()) {
+            StringBuilder fullLine = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fullLine.append(line);
+            }
+            JsonElement jsonElement =  new JsonParser().parse(String.valueOf(fullLine));
+            String json = gson.toJson(jsonElement);
+            writer.println(json);
+            writer.flush();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
